944658b79b051c167a43df0c1fdc624f3753519f
Wilhelm Willie <wilhelmwillie@gmail.com>
2017-06-12 21:35:25 -0700

    Created Angular service thru angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
+import { GithubApiService } from './github-api.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,9 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  	GithubApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
