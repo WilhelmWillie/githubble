38bfe4accbb65d49f7d28d744e112c90f34ea3be
Wilhelm Willie <wilhelmwillie@gmail.com>
2017-06-16 14:25:28 -0400

    On init, search bar will fill placeholder with a random programming topic

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 import { GithubApiService } from '../github-api.service';
 
@@ -9,11 +9,33 @@ import { Repository } from '../repository';
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
   private query : string;
+  private placeholder : string;
+
   @Output() search : EventEmitter<Repository[]> = new EventEmitter<Repository[]>();
 
   constructor(private githubApiService : GithubApiService) { }
+
+  ngOnInit() {
+    const placeholders = [
+      'machine learning',
+      'big data',
+      'iot',
+      'neural networks',
+      'data mining',
+      'blockchain',
+      'cryptography',
+      'embedded systems',
+      'c++',
+      'java',
+      'python',
+      'javascript',
+      'sql'
+    ];
+
+    this.placeholder = placeholders[Math.floor(Math.random() * placeholders.length)];
+  }
 
   searchRepos() {
     // Make call to GitHub API and emit data to parent
